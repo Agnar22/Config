1db06fd5606f26ac336b7afb70c9f5baaff0b325
Agnar22 <agnar.bjornstad@gmail.com>
2022-01-12 20:11:30 +0100

    Add bash script for download of packages.

---
+++ packages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install vim tmux network-manager git tig openvpn -y
+
+git config --global user.name "Agnar22"
+git config --global user.email "agnar.bjornstad@gmail.com"
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+
+# VS Code
+sudo snap install code --classic 
+
+# Pycharm
+sudo snap install pycharm-professional --classic
+
+# Slack 
+sudo snap install slack --classic
+
+# Discord
+sudo snap install discord
+
+
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Terminal.desktop', 'pycharm-professional_pycharm-professional.desktop', 'code_code.desktop', 'slack_slack.desktop', 'discord_discord.desktop', 'org.gnome.Nautilus.desktop']"
+
