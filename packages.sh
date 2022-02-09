81b9ed5eb671b41dd752022385a55fa14543648a
Agnar22 <agnar.bjornstad@gmail.com>
2022-02-09 14:45:07 +0100

    Add sublime text.

--- packages.sh
+++ packages.sh
@@ -10,7 +10,6 @@ git config --global user.email "agnar.bjornstad@gmail.com"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-
 # VS Code
 sudo snap install code --classic 
 
@@ -23,7 +22,10 @@ sudo snap install slack --classic
 # Discord
 sudo snap install discord
 
+# Sublime text
+sudo snap install sublime-text --classic
 
+# Set favorite-apps
 gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Terminal.desktop', 'pycharm-professional_pycharm-professional.desktop', 'code_code.desktop', 'slack_slack.desktop', 'discord_discord.desktop', 'org.gnome.Nautilus.desktop']"
 
 # Swap caps<->escape
