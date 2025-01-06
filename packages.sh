4e3b5ab689e6261579b0d28da3579bececdf0e36
Agnar Martin Bjørnstad <agnar.bjornstad@gmail.com>
2025-01-06 10:26:27 +0100

    feat: install docker

--- packages.sh
+++ packages.sh
@@ -38,3 +38,19 @@ gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnom
 
 # Swap caps<->escape
 setxkbmap -option caps:swapescape
+
+
+# Install docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
